a02c8e2e805c8ff359f9d8b3f03dc1e46fa1e1e2
FuryLusifer <piyushkayastha2@gmail.com>
2025-10-30 11:54:30 +0545

    modified payment info

--- src/pages/admin/Orders.tsx
+++ src/pages/admin/Orders.tsx
@@ -370,6 +370,7 @@ const Orders = () => {
     yPos += 5;
     doc.setFont("helvetica", "normal")
     doc.text(`Delivery Charge: Rs. ${order.total_amount.toFixed(2)}`, 120, yPos);
+    yPos += 5;
     doc.setFont("helvetica", "bold");
     doc.text(`TOTAL AMOUNT: Rs. ${order.total_amount.toFixed(2)}`, 120, yPos);
     
